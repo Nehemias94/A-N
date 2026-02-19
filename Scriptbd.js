43611a52fd574b3a021d8bea7a4287e85de43557
Nehemias Zepeda <119623680+Nehemias94@users.noreply.github.com>
2026-02-19 11:06:30 -0600

    Update Scriptbd.js

--- Scriptbd.js
+++ Scriptbd.js
@@ -1,45 +1,3 @@
-// 🚫 Deshabilitar clic derecho
-document.addEventListener("contextmenu", function (e) {
-  e.preventDefault();
-});
-
-// 🚫 Bloquear teclas de inspección
-document.addEventListener("keydown", function (e) {
-  // F12
-  if (e.key === "F12") {
-    e.preventDefault();
-  }
-
-  // Ctrl + Shift + I
-  if (e.ctrlKey && e.shiftKey && e.key === "I") {
-    e.preventDefault();
-  }
-
-  // Ctrl + Shift + J
-  if (e.ctrlKey && e.shiftKey && e.key === "J") {
-    e.preventDefault();
-  }
-
-  // Ctrl + U (ver código fuente)
-  if (e.ctrlKey && e.key === "u") {
-    e.preventDefault();
-  }
-});
-
-// ⚠️ Detectar si DevTools está abierto
-setInterval(function () {
-  const anchoDiferencia = window.outerWidth - window.innerWidth;
-  const altoDiferencia = window.outerHeight - window.innerHeight;
-
-  if (anchoDiferencia > 160 || altoDiferencia > 160) {
-    document.body.innerHTML = `
-      <div style="display:flex;justify-content:center;align-items:center;height:100vh;font-family:sans-serif;">
-        <h2>Inspección detectada 🚫</h2>
-      </div>
-    `;
-  }
-}, 1000);
-
 /* =========================
    CUENTA REGRESIVA
 ========================= */
@@ -208,7 +166,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       mensajeRegalo.style.display = 'block';
     }
 
-    if (data.numero_mesa && data.numero_mesa > 0) {
+    if (data.numero_mesa && data.numero_mesa > 0 || data.confirmado === true) {
       numMesa.textContent = `🪑 Tu mesa asignada es la número ${data.numero_mesa}`;
       msjeMesa.style.display = 'block';
       msjeMesa.removeAttribute('aria-hidden');
@@ -641,6 +599,7 @@ async function confirmarNoAsistencia() {
     }
   }
 }
+
 
 
 
